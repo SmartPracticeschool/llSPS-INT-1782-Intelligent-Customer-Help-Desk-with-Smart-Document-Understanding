3e4c3da1b25175089c78caf80e59ae20d30ed2ce
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2020-06-15 03:17:05 +0530

    updated cloudfunction.js

--- cloudfunction.js
+++ cloudfunction.js
@@ -1,31 +1,7 @@
-/**
-  *
-  * @param {object} params
-  * @param {string} params.iam_apikey
-  * @param {string} params.url
-  * @param {string} params.username
-  * @param {string} params.password
-  * @param {string} params.environment_id
-  * @param {string} params.collection_id
-  * @param {string} params.configuration_id
-  * @param {string} params.input
-  *
-  * @return {object}
-  *
-  */
-
 const assert = require('assert');
 const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 
-/**
-  *
-  * main() will be run when you invoke this action
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
+
 function main(params) {
   return new Promise(function (resolve, reject) {
 
@@ -61,4 +37,4 @@ function main(params) {
       return resolve(data);
     });
   });
-}`
+}
